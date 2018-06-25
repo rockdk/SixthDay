6e27f69e1eecfb765c663c07bbb95276d06054f4
rockdk <deepak875618@gmail.com>
2018-06-25 21:57:15 +0530

    Initial commit

---
+++ app/src/main/java/com/champ/web/sixthday/MainActivity.java
@@ -0,0 +1,117 @@
+package com.champ.web.sixthday;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt_normal,bt_custom,
+            bt_spin,bt_hori;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt_normal=findViewById(R.id.normal);
+        bt_custom=findViewById(R.id.custom);
+        bt_hori=findViewById(R.id.hori);
+        bt_spin=findViewById(R.id.spiner);
+        bt_normal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                AlertDialog.Builder alert=
+                        new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Good Morning");
+                alert.setMessage("Hello");
+
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,
+                                "Yes",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,
+                                "No",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                alert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,
+                                "Cancel",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+                alert.show();
+
+            }
+        });
+
+       //custom
+
+        bt_custom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog=new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.custom);
+                Button bt=dialog.findViewById(R.id.button);
+                bt.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+        //spiner
+
+        bt_spin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressDialog progressDialog=
+                        new ProgressDialog(MainActivity.this);
+
+                progressDialog.setTitle("Please Wait");
+                progressDialog.setMessage("please wait Content Loading");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progressDialog.dismiss();
+                progressDialog.setCancelable(true);
+                progressDialog.show();
+            }
+        });
+
+        bt_hori.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressDialog progressDialog=
+                        new ProgressDialog(MainActivity.this);
+
+                progressDialog.setTitle("Please Wait");
+                progressDialog.setMessage("please wait Content Loading");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressDialog.show();
+            }
+        });
+    }
+}
